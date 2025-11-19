1830d1c829af1edaac8e772935c0bcddb8c59d09
yanxi.wang <yanxi.wang@transwarp.io>
2025-11-19 17:05:51 +0800

    fix: npm run build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/整体介绍/介绍">
+            LTMDB Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
