0ae64bca89c8b0ca4fac95412f62a46b6e23b9d3
yanxi.wang <yanxi.wang@transwarp.io>
2025-11-24 18:40:12 +0800

    fix: hide Giscus on blog home

--- src/theme/BlogPostItem/Footer/index.tsx
+++ src/theme/BlogPostItem/Footer/index.tsx
@@ -2,15 +2,20 @@ import React, {type ReactNode} from 'react';
 import Footer from '@theme-original/BlogPostItem/Footer';
 import type FooterType from '@theme/BlogPostItem/Footer';
 import type {WrapperProps} from '@docusaurus/types';
+import { useLocation } from '@docusaurus/router';
 import GiscusComponent from '@site/src/components/Giscus';
 
 type Props = WrapperProps<typeof FooterType>;
 
 export default function FooterWrapper(props: Props): ReactNode {
+  const location = useLocation();
+  console.log(location);
+  const isBlogHomePage = location.pathname === '/ltmdb-docs/blog';
+
   return (
     <>
       <Footer {...props} />
-      <GiscusComponent />
+      {!isBlogHomePage && <GiscusComponent />}
     </>
   );
 }
